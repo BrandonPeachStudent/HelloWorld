f05ed41c3c35bc0ec6cd37e4a3e36e7e5e03e56c
Brandon Peach <brandon.peach.2022@student.lhschools.org>
2020-02-20 10:51:31 -0500

    intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,19 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("My name is ");
+        System.out.println("Brandon");
+
+        //Print three more lines about what you did last summer
+
+        System.out.println("This past summer I did a lot of boring and exciting things");
+        System.out.print("I spent time with friends ");
+        System.out.println("and family");
+        System.out.println("I completed summer assignments for school");
+
+        System.out.println("I played a lot of soccer");
+    }
+}
